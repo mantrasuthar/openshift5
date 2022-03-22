dcb3f5ec2a0b419bb4aeb67f0f55c6ae11630405
mantrasuthar <mantrasuthar@gmail.com>
2022-03-22 15:14:56 -0600

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,8 +1,4 @@
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Pushpak',1);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Nayan',2);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Chait',3);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rutuja',4);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Tanmay',5);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('B1',1003);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('B2',1104);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('B3',1005);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Mantra',1);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Suraj',2);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Tej',3);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rudraraj',4);
